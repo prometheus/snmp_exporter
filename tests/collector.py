f85d28f406d48fa24177a97e089428fe56248ddb
Brian Brazil <brian.brazil@robustperception.io>
2016-01-30 14:32:54 +0000

    Pad right when 0s are stripped from an oid.
    Fix tests to work with netsnmp.

--- tests/collector.py
+++ tests/collector.py
@@ -1,21 +1,21 @@
 import unittest
 
 from snmp_exporter.collector import parse_indexes
-from pysnmp.proto.rfc1902 import Counter32, ObjectName, OctetString
 
 class TestCollector(unittest.TestCase):
 
-  def setUp(self):
-    self.oids = {ObjectName('1.2.3.4'): OctetString('eth0')}
 
   def test_parse_indexes(self):
-    self.assertEqual({}, parse_indexes((), [], [], self.oids))
+    oids = {(1, 2, 3, 4): 'eth0'}
+    self.assertEqual({}, parse_indexes((), [], [], oids))
     self.assertEqual({'l': '4'}, 
-        parse_indexes((4,), [{'labelname': 'l', 'type': 'Integer32'}], [], self.oids))
+        parse_indexes((4,), [{'labelname': 'l', 'type': 'Integer32'}], [], oids))
     self.assertEqual({'l': 'eth0'}, 
         parse_indexes((4,), [{'labelname': 'l', 'type': 'Integer32'}], 
-                      [{'labels': ['l'], 'labelname': 'l', 'oid': '1.2.3'}], self.oids))
+                      [{'labels': ['l'], 'labelname': 'l', 'oid': '1.2.3'}], oids))
     self.assertEqual({'a': '3', 'b': '4', 'l': 'eth0'}, 
         parse_indexes((3, 4,), [{'labelname': 'a', 'type': 'Integer32'}, {'labelname': 'b', 'type': 'Integer32'}], 
-                      [{'labels': ['a', 'b'], 'labelname': 'l', 'oid': '1.2'}], self.oids))
+                      [{'labels': ['a', 'b'], 'labelname': 'l', 'oid': '1.2'}], oids))
+    self.assertEqual({'l': '0'},
+        parse_indexes((), [{'labelname': 'l', 'type': 'Integer32'}], [], {}))
 
