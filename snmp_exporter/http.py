prometheus/snmp_exporter

commit ad4689898928a38d46372e90231a9d5d6a2a3c85

Fix python3 compatibility problems

snmp_exporter/http.py
@@ -1,14 +1,19 @@
 import traceback
-import urlparse
-from BaseHTTPServer import BaseHTTPRequestHandler
-from BaseHTTPServer import HTTPServer
-from SocketServer import ForkingMixIn
-
 import yaml
 from prometheus_client import CONTENT_TYPE_LATEST
+from .collector import collect_snmp
 
-
-from collector import collect_snmp
+try:
+    from urlparse import urlparse
+    from BaseHTTPServer import BaseHTTPRequestHandler
+    from BaseHTTPServer import HTTPServer
+    from SocketServer import ForkingMixIn
+except ImportError:
+    # python3 renamed those modules, try new names
+    from urllib.parse import urlparse
+    from http.server import BaseHTTPRequestHandler
+    from http.server import HTTPServer
+    from socketserver import ForkingMixIn
 
 
 class ForkingHTTPServer(ForkingMixIn, HTTPServer):

scripts/snmp_exporter
@@ -4,7 +4,7 @@ from snmp_exporter.http import start_http_server
 
 if __name__ == '__main__':
   if len(sys.argv) not in [1, 2, 3]:
-    print "Usage: snmp_exporter [config_file] [port]"
+    print("Usage: snmp_exporter [config_file] [port]")
     sys.exit(1)
 
   if len(sys.argv) >= 2:

commit 94736fe3d8a354379b1b33ea27ab2142c24e7775

Fix urlparse imports broken in commit ad46898

snmp_exporter/http.py
@@ -4,13 +4,13 @@ from prometheus_client import CONTENT_TYPE_LATEST
 from .collector import collect_snmp
 
 try:
-    from urlparse import urlparse
+    import urlparse
     from BaseHTTPServer import BaseHTTPRequestHandler
     from BaseHTTPServer import HTTPServer
     from SocketServer import ForkingMixIn
 except ImportError:
     # python3 renamed those modules, try new names
-    from urllib.parse import urlparse
+    import urllib.parse as urlparse
     from http.server import BaseHTTPRequestHandler
     from http.server import HTTPServer
     from socketserver import ForkingMixIn