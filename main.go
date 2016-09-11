65a5b4f18941449031fa0d02f6d38dae5fa7995d
Brian Brazil <brian.brazil@robustperception.io>
2016-09-11 07:50:10 +0100

    Add HTTP, more refactoring.

--- main.go
+++ main.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net"
+	"net/http"
 	"strconv"
 	"strings"
 	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/common/log"
 	"github.com/soniah/gosnmp"
 	"gopkg.in/yaml.v2"
@@ -20,7 +23,7 @@ var (
 		"Path to configuration file.",
 	)
 	listenAddress = flag.String(
-		"web.listen-address", ":9104",
+		"web.listen-address", ":9116",
 		"Address to listen on for web interface and telemetry.",
 	)
 )
@@ -152,25 +155,46 @@ func buildMetricTree(metrics []*Metric) *MetricNode {
 	return metricTree
 }
 
-func main() {
-	flag.Parse()
+type collector struct {
+	target string
+	module *Module
+}
 
-	cfg, err := LoadFile(*configFile)
+// Describe implements Prometheus.Collector.
+func (c collector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- prometheus.NewDesc("dummy", "dummy", nil, nil)
+}
+
+func PduToSample(metric *Metric, pdu *gosnmp.SnmpPDU) prometheus.Metric {
+	return prometheus.MustNewConstMetric(prometheus.NewDesc(metric.Name, "", []string{"label"}, nil),
+		prometheus.UntypedValue,
+		float64(gosnmp.ToBigInt(pdu.Value).Int64()),
+		pdu.Name,
+	)
+}
+
+// Collect implements Prometheus.Collector.
+func (c collector) Collect(ch chan<- prometheus.Metric) {
+	start := time.Now()
+	pdus, err := ScrapeTarget(c.target, c.module)
 	if err != nil {
-		log.Errorf("Error parsing config file: %s", err)
+		log.Errorf("Error scraping target %s: %s", c.target, err)
 		return
 	}
-	_ = cfg
-
-	module := (*cfg)["default"]
-	metricTree := buildMetricTree(module.Metrics)
-
-	pdus, err := ScrapeTarget("192.168.1.2", module)
-	oidToPdu := make(map[string]*gosnmp.SnmpPDU, len(pdus))
+	ch <- prometheus.MustNewConstMetric(
+		prometheus.NewDesc("snmp_scrape_walk_duration_seconds", "Time SNMP walk/bulkwalk took.", nil, nil),
+		prometheus.GaugeValue,
+		float64(time.Since(start).Seconds()))
+	ch <- prometheus.MustNewConstMetric(
+		prometheus.NewDesc("snmp_scrape_pdus_returned", "PDUs returned from walk.", nil, nil),
+		prometheus.GaugeValue,
+		float64(len(pdus)))
+	oidToPdu := make(map[string]gosnmp.SnmpPDU, len(pdus))
 	for _, pdu := range pdus {
-		oidToPdu[pdu.Name[1:]] = &pdu
+		oidToPdu[pdu.Name[1:]] = pdu
 	}
 
+	metricTree := buildMetricTree(c.module.Metrics)
 	// Look for metrics that match each pdu.
 PduLoop:
 	for oid, pdu := range oidToPdu {
@@ -184,9 +208,53 @@ PduLoop:
 			}
 			if head.metric != nil {
 				// Found a match.
-				fmt.Printf("Metric: %s Value: %s Remaining Oid: %s\n", head.metric.Name, pdu.Value, oidList[len(head.oidList):])
+				ch <- PduToSample(head.metric, &pdu)
 				break
 			}
 		}
 	}
+	ch <- prometheus.MustNewConstMetric(
+		prometheus.NewDesc("snmp_scrape_duration_seconds", "Total SNMP time scrape took (walk and processing).", nil, nil),
+		prometheus.GaugeValue,
+		float64(time.Since(start).Seconds()))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	cfg, err := LoadFile(*configFile)
+	if err != nil {
+		msg := fmt.Sprintf("Error parsing config file: %s", err)
+		http.Error(w, msg, 400)
+		log.Errorf(msg)
+		return
+	}
+
+	target := r.URL.Query().Get("target")
+	if target == "" {
+		http.Error(w, "'target' parameter must be specified", 400)
+		return
+	}
+	moduleName := r.URL.Query().Get("module")
+	if moduleName == "" {
+		moduleName = "default"
+	}
+	module, ok := (*cfg)[moduleName]
+	if !ok {
+		http.Error(w, fmt.Sprintf("Unkown module '%s'", module), 400)
+		return
+	}
+
+	registry := prometheus.NewRegistry()
+	collector := collector{target: target, module: module}
+	registry.MustRegister(collector)
+	// Delegate http serving to Promethues client library, which will call collector.Collect.
+	h := promhttp.HandlerFor(registry, promhttp.HandlerOpts{})
+	h.ServeHTTP(w, r)
+}
+
+func main() {
+	flag.Parse()
+
+	http.Handle("/metrics", promhttp.Handler()) // Normal metrics endpoint for SNMP exporter itself.
+	http.HandleFunc("/snmp", handler)           // Endpoint to do SNMP scrapes.
+	log.Fatal(http.ListenAndServe(*listenAddress, nil))
 }
