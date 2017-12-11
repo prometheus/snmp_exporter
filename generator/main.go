b44f5a9bf7748ca2b9e1036a32549f7f93d39dbe
Tanton <tantonat@amazon.com>
2017-12-11 10:11:44 -0600

    added output-dir command line flag so that you can specify a location to write the resulting snmp.yaml file

--- generator/main.go
+++ generator/main.go
@@ -15,6 +15,7 @@ import (
 
 // Generate a snmp_exporter config and write it out.
 func generateConfig(nodes *Node, nameToNode map[string]*Node) {
+	outputPath := fmt.Sprintf("%s/snmp.yml", *outputDir)
 	content, err := ioutil.ReadFile("generator.yml")
 	if err != nil {
 		log.Fatalf("Error reading yml config: %s", err)
@@ -46,7 +47,7 @@ func generateConfig(nodes *Node, nameToNode map[string]*Node) {
 		log.Fatalf("Error parsing generated config: %s", err)
 	}
 
-	f, err := os.Create("snmp.yml")
+	f, err := os.Create(outputPath)
 	if err != nil {
 		log.Fatalf("Error opening output file: %s", err)
 	}
@@ -54,13 +55,14 @@ func generateConfig(nodes *Node, nameToNode map[string]*Node) {
 	if err != nil {
 		log.Fatalf("Error writing to output file: %s", err)
 	}
-	log.Infof("Config written to snmp.yml")
+	log.Infof("Config written to %s", outputPath)
 }
 
 var (
 	generateCommand    = kingpin.Command("generate", "Generate snmp.yml from generator.yml")
 	parseErrorsCommand = kingpin.Command("parse_errors", "Debug: Print the parse errors output by NetSNMP")
 	dumpCommand        = kingpin.Command("dump", "Debug: Dump the parsed and prepared MIBs")
+	outputDir          = kingpin.Flag("output-dir", "Directory to write resulting config file").Default("/opt").Short('o').String()
 )
 
 func main() {
