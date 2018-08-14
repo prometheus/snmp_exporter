4cbe59f124f6b8bce48913c7acaa1fb74eb8c462
Ben Kochie <superq@gmail.com>
2018-08-14 17:16:04 +0200

    Refactor type walkNode in generator/tree.go.
    Signed-off-by: Ben Kochie <superq@gmail.com>

--- generator/tree.go
+++ generator/tree.go
@@ -90,8 +90,9 @@ func prepareTree(nodes *Node) map[string]*Node {
 	// is technically only ASCII.
 	displayStringRe := regexp.MustCompile(`^\d+[at]$`)
 
-	// Set type on MAC addresses and strings.
+	// Apply various tweaks to the types.
 	walkNode(nodes, func(n *Node) {
+		// Set type on MAC addresses and strings.
 		// RFC 2579
 		switch n.Hint {
 		case "1x:":
@@ -106,17 +107,13 @@ func prepareTree(nodes *Node) map[string]*Node {
 		if n.TextualConvention == "DisplayString" {
 			n.Type = "DisplayString"
 		}
-	})
 
-	// Promote Opaque Float/Double textual convention to type.
-	walkNode(nodes, func(n *Node) {
+		// Promote Opaque Float/Double textual convention to type.
 		if n.TextualConvention == "Float" || n.TextualConvention == "Double" {
 			n.Type = n.TextualConvention
 		}
-	})
 
-	// Convert RFC 2579 DateAndTime textual conversion to type.
-	walkNode(nodes, func(n *Node) {
+		// Convert RFC 2579 DateAndTime textual conversion to type.
 		if n.TextualConvention == "DateAndTime" {
 			n.Type = "DateAndTime"
 		}
